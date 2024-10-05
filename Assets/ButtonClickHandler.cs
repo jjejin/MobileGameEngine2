1869ea811f588009f32a5cb180f6ed34ba60b85a
jjejin <juyejk020775@gmail.com>
2024-10-06 00:52:40 +0900

    과제 제출 이렇게 하는거 맞나요

---
+++ Assets/ButtonClickHandler.cs
@@ -0,0 +1,18 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ButtonClickHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+
+    public void OnButtonClick(string message)
+    {
+        Debug.Log(message);
+    }
+
+    // Update is called once per frame
+    
+}
